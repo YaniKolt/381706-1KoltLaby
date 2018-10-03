28f7dd8731ccfc40b8cdfedab79d5a465dc02e92
YaniKolt <belovidyanochka@gmail.com>
2018-10-03 10:25:16 +0300

    Что-то добавил2и

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include "Header.h"
 using namespace std;
 
-void main()
+int main()
 {
-	int c = a(2);
-	double k = fac(3);
+
   cout << "\nHello, world\n";
-  cout << k<< "\n";
-  star(3);
-  cout << "\n";
+  return 0;
 }
