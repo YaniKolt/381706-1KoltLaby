d45d4ed0113c47e5ed4e42725416e58f9643f404
381706-1YaniKo <43132462+YaniKolt@users.noreply.github.com>
2019-02-13 23:20:12 +0300

    Update ExceptionLib.h

--- ExceptionLib/ExceptionLib.h
+++ ExceptionLib/ExceptionLib.h
@@ -8,6 +8,13 @@ class MyException
 private: 
 string str; 
 public: 
-MyException(string _str) : str(_str) {} 
-void Print() { cout « "\nWarning! \nMessage: " « str « endl; } 
-};
+MyException(std::string _str); 
+void Print(); 
+}; 
+
+MyException::MyException(std::string _str) : str(_str) {} 
+
+void MyException::Print() 
+{ 
+cout « "\nWarning! \nMessage: " « str « endl; 
+}
